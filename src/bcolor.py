42cabba1d8e052535dfe4198c600fe2ff1b38296
byteofsoren <r00tr4t@gmail.com>
2021-05-14 23:43:35 +0200

    t test is worked on
    Signed-off-by: byteofsoren <r00tr4t@gmail.com>

--- src/bcolor.py
+++ src/bcolor.py
@@ -4,12 +4,20 @@ class bcolors:
     OKCYAN = '\033[96m'
     OK = '\033[92m'
     WARN = '\033[93m'
-    ERR = '\033[91m'
+    ERR = '\033[38;5;9m'
     END = '\033[0m'
     BOLD = '\033[1m'
     UND = '\033[4m'
     INF = '\033[38;5;81m'
     class SIGN:
         OK = "\033[92m[OK]\033[0m"
+        NO = "\033[93m[NO]\033[0m"
         FAIL = "\033[91m[FAIL]\033[0m"
+        FIXED  = "\033[96m[FIXED]\033[0m"
+    def tfcolor(t):
+        if t:
+            return "\033[94m[True]\033[0m"
+        else:
+            return "\033[38;5;202m[False]\033[0m"
+
 
--- src/readset.py
+++ src/readset.py
@@ -25,6 +25,8 @@ import pandas as pd
 # import matplotlib as mpl
 # import matplotlib.image as mplimg
 import pylab
+# from scipy.stats import f as f_test
+from scipy import stats
 from matplotlib.cbook import get_sample_data
 from typing import Type
 from glob import glob
@@ -45,6 +47,9 @@ log.addHandler(f_log)
 log.setLevel(logging.INFO)
 # == END ===
 
+
+# Pandas display formatting.
+pd.options.display.float_format='{:.3f}'.format
 
 
 class dataset():
@@ -106,14 +111,37 @@ class dataset():
         #     raise e
 
     def load_anatations(self):
+        """
+        This function loads the annotated data from the multiple csv files.
+        In each set there is a 'set.yaml' containing the sub dict 'annotations'.
+        That sub dict contains the following structure.
+          - user:
+            path:
+            column_names:
+            delimiter:
+            type:
+        The user specifies what kind of user produced the data.
+        Possible entry's is "Human", "OpenPose" and "Test"
+        Then when this function ends it combines the annotations from multiple
+        CSV files in to one with an extra column that contains the for mentioned user.
+
+        ---------------------------------------------------------
+        | n |  user | label | u | v | filename | width | height |
+        ---------------------------------------------------------
+        |.
+
+        """
+        # The following labda function is used for translating
+        # languages from one language to a other
+        # a  = lambda inlabel, outlabel: csvfile.loc[(df.label == inlabel),'label']= outlabel
         setdir:str = self.setdir
-        atconf = self.setconf['anatations']
+        atconf = self.setconf['annotations']
         bc = lambda x: f"{bcolors.INF}{x}{bcolors.END}"
         log.info(f"length = {bc(len(atconf))}")
         data = {}
         for csvset in atconf:
             csvpath = Path(f"{setdir}/{csvset['path']}")
-            csvname = csvset['name']
+            csvname = csvset['user'] # Use this to identify user or openpose
             csvcols = csvset['column_names']
             csvtype = csvset['type']
             csvdeli = csvset['delimiter']
@@ -126,16 +154,25 @@ class dataset():
                         delimiter=csvdeli,
                         names=csvcols,
                         ) # Removed the index_col to make a merged df
-                data[str(csvname)] = csvfile
-                data[str(csvname)]['label'] = data[str(csvname)]['label'].str.title()
+                if not csvtype == 0:
+                    trans:dict = self._conf['set_translation'][csvtype]
+                    print(trans)
+                    for lkey in trans.keys():
+                        csvfile.loc[(csvfile.label == lkey),'label'] = trans[lkey]
+                data[str(csvset['path'])] = csvfile
+                data[str(csvset['path'])]['label'] = data[str(csvset['path'])]['label'].str.title()
+                data[str(csvset['path'])]['user'] = str(csvname)
             else:
                 t = f"file: {str(csvpath)} does {bcolors.WARN}NOT exist{bcolors.END}."
                 log.info(t)
                 raise ValueError(t)
-        [data[key].insert(0,"user",key,True) for key in data.keys()]
-        self.indata = pd.concat([data[key] for key in data.keys()])
+        # Create total indata table using pandas concat function.
+        self.indata = pd.concat([data[key] for key in data.keys()], ignore_index=True)
+        # print(self.indata)
+        log.info(self.indata)
+        # breakpoint()
 
-    def select_data(self, andarg:dict, rest_col=[]):
+    def select_data(self, andarg:dict, rest_col=[], df=None):
         """
         Does a SQL like selection based on the dict argumnt
         Supose
@@ -149,8 +186,10 @@ class dataset():
 
         :param dict andarg: Dict that selects rows in df
         :param list rest_col: List for selecting columns
+        :param pd.DataFrame df: can be used to select data from other df
         """
-        df = self.indata
+        if df is None:
+            df=self.indata
         ret = df.loc[np.all(df[list(andarg)] == pd.Series(andarg), axis=1)]
         if not len(rest_col) > 0:
             return ret
@@ -159,12 +198,302 @@ class dataset():
 
 
     def analyse_feature(self, andarg, rest_col=[]):
+        breakpoint()
         dd = self.select_data(andarg, rest_col)
         med:pd.Series    = dd.median()
         std:pd.Series    = dd.std()
         cov:pd.DataFrame = dd.cov()
+        n:int            = dd.shape[0]
         log.info(f"median:\n{med}\nstd:\n{std}\ncovariance:\n{cov}")
-        return med,std,cov
+        return med,std,cov,n
+
+    def error_calulation(self,  frac=0.5):
+        """
+        Runs a statistical analysis based on where the picture was taken.
+        :param float frac: Divider of human data
+        """
+        naming=self._conf['ftest']['naming']
+
+        dh = self.select_data({'user':"Human"}, ['user','filename','label', 'u','v'])
+
+        do_openp = self.select_data({'user':"OpenPose"}, ['user', 'filename','label', 'u','v'])
+        # breakpoint()
+        labels:list = dh['label'].unique()
+        fnames:list = dh['filename'].unique()
+        if isinstance(labels,np.ndarray):
+            labels = labels.tolist()
+        if isinstance(fnames,np.ndarray):
+            fnames = fnames.tolist()
+        for l in do_openp['label'].unique():
+            if not l in labels:
+                labels.append(l)
+                log.info(f"label {l} not in {bcolors.SIGN.FIXED}")
+            else:
+                log.info(f"label {l} was in {bcolors.SIGN.OK} ")
+        for f in do_openp['filename'].unique():
+            if not f in fnames:
+                fnames.append(f)
+                log.info(f"filename {f} not in {bcolors.SIGN.FIXED}")
+            else:
+                log.info(f"filename {f} was in {bcolors.SIGN.OK} ")
+
+        # Random divider of human data.
+        # Total error calculation.
+        # Total f test
+        # Total t test
+        # error calculation for each South, west, north and east
+        # f test for each South, West, North and East
+        # t test for each South, West, North and East
+        #
+        # 1. Random divider of human data
+        dh_sampl:pd.DataFrame = dh.sample(frac=(1-frac))
+        dh_human:pd.DataFrame = dh.drop(dh_sampl.index)
+        # dh_human:pd.DataFrame = dh.sample(frac=(1-frac))
+        # dh_sampl:pd.DataFrame = dh.drop(dh_human.index)
+
+
+        # Build median mu matrix for each label in each image
+        # The mumtx is the error matrix with the shape:
+        #           foot | head | arm |
+        # 2343.jpg
+        # 6543.jpg
+        openp_error_df= pd.DataFrame(columns=labels, index=fnames)
+        human_error_df= pd.DataFrame(columns=labels, index=fnames)
+        # breakpoint()
+        error_degdf_df = pd.DataFrame(columns=[naming['hd'],naming['od']])
+
+        self.unique = {'columns':labels, 'index':fnames}
+        # How to update test.at['2134.jpg','foot']= 34
+        terr = lambda mux,muy,tx,ty : np.sqrt((mux-tx)**2 + (muy-ty)**2)
+        getsub = lambda df: self.select_data({'filename':file,'label':label},['u','v'],df=df)
+        imgloc = self.setconf['imgloc']
+        self.unique['imgloc'] = imgloc
+        # imgloc stores the approximated positon of where the camera is located.
+        # imgloc['2543.jpg'] -> South means that the picture was taken from
+        # the feats wile North means form the head.
+        for file in fnames:
+            for label in labels:
+                log.info(f"file={file}, label={label}")
+                mean_df:pd.DataFrame = getsub(dh_human)
+                # log.info(f"mean_df shape = {mean_df.shape}")
+                log.info(f"mean_df = \n{mean_df}")
+                sampl_df:pd.DataFrame = getsub(dh_sampl)
+                # log.info(f"samp_df shape = {sampl_df.shape}")
+                log.info(f"samp_df = \n{sampl_df}")
+                # breakpoint()
+                openp_df:pd.DataFrame = getsub(do_openp)
+                thuman_mean = mean_df.mean()
+                # log.warn(f"thuman_mean shape = {thuman_mean.shape}")
+                # Store the degres of freedom
+
+                tdefdf = list()
+                if label in error_degdf_df.index:
+                    tdef_df = error_degdf_df.loc[label]
+                else:
+                    tdef_df = [0,0]
+                tdefdf.append(mean_df.shape[0]   + tdef_df[0])
+                tdefdf.append(openp_df.shape[0] + tdef_df[1])
+                error_degdf_df.loc[label] = tdefdf
+
+
+                thsamp_mean = sampl_df.mean()
+                topenp_mean = openp_df.mean()
+                # Median calulation
+                mux,muy = thuman_mean.values.tolist()
+
+                # Our test cases.
+                # Human sample
+                tx,ty = thsamp_mean.values.tolist()
+                perr=terr(mux,muy,tx,ty)
+                human_error_df.at[file,label] = perr
+                human_error_df.at[file,"Direction"] = imgloc [file]
+                # log.info(f"OK human error {perr:.2f}")
+                # OpenPose sample
+                tx,ty = topenp_mean.values.tolist()
+                perr=terr(mux,muy,tx,ty)
+                openp_error_df.at[file,label] = perr
+                openp_error_df.at[file,"Direction"] = imgloc[file]
+                # log.info(f"OK OpenPose error {perr:.2f}")
+
+        errordict = dict()
+        # breakpoint()
+        print("Cumulative error for a human:")
+        errordict[naming['hm']] = human_error_df.mean()
+        errordict[naming['hv']] = human_error_df.var()
+        print("Cumulative error for a OpenPose:")
+        errordict[naming['om']] = openp_error_df.mean()
+        errordict[naming['ov']] = openp_error_df.var()
+        errordf = pd.DataFrame(errordict)
+
+        # Cumdict creates an row for mean
+        # and variance to apply to error dict.
+        cumdict = dict() # <-- Important data found here:
+        cumdict[naming['hm']] = human_error_df.mean().mean()
+        cumdict[naming['hv']]  = human_error_df.mean().var()
+        cumdict[naming['om']] = openp_error_df.mean().mean()
+        cumdict[naming['ov']]  = openp_error_df.mean().var()
+        s = pd.Series(cumdict,name='Total mean/variance:')
+        self.errordf = errordf.append(s,ignore_index=False)
+        print(errordf)
+
+        # directional error calculation.
+        # human_direction=dict()
+        # openp_direction=dict()
+        direction_error= pd.DataFrame(
+                columns=[
+                    naming['hm'],
+                    naming['hv'],
+                    naming['om'],
+                    naming['ov']
+                    ],
+                index=set(imgloc.values())) # <-- More important data found here
+        direction_degdf_df = pd.DataFrame(
+                columns=[
+                    naming['hd'],
+                    naming['od'],
+                    ],
+                index=set(imgloc.values()))
+        for direction in set(imgloc.values()):
+            human_direction = self.select_data({'Direction':direction},df=human_error_df)
+            openp_direction = self.select_data({'Direction':direction},df=openp_error_df)
+
+            direction_error.at[direction,naming['hm']] = human_direction.mean().mean()
+            direction_error.at[direction,naming['hv']] = human_direction.mean().var()
+            direction_error.at[direction,naming['om']] = openp_direction.mean().mean()
+            direction_error.at[direction,naming['ov']] = openp_direction.mean().var()
+
+
+            temp_deg = list()
+            temp_deg.append(human_direction.count().sum())
+            temp_deg.append(openp_direction.count().sum())
+            direction_degdf_df.loc[direction] = temp_deg
+
+
+        print(direction_error)
+        self.direction_error_df:pd.DataFrame = direction_error
+        self.error_df:pd.DataFrame  = errordf
+        self.error_degdf_df:pd.DataFrame = error_degdf_df
+        self.human_error_df = human_error_df
+        self.openp_error_df = openp_error_df
+        print(error_degdf_df)
+        print(direction_degdf_df)
+        self.direction_degdf_df = direction_degdf_df
+
+
+    def error_t_test(self, user1:str='Human',user2='OpenPose'):
+        """
+        Error t-test does both a f-test and t-test on the given data in
+        the pandas frames:
+            self.direction_error_df
+            self.error_df
+
+        The dict self.unique contains:
+            "index":
+                "1233.jpg"
+                "2332.jpg"
+                "1323.jpg"
+            "columns"
+                "Rsholder"
+                "Relbow"...
+            "imgloc" ie image location:
+                "123.jpg":"South"
+                "124.jpg":"West"
+                "131,jpg":"North"
+                "125.jpg":"East"
+        """
+        # Naming provides synonym's to as effort to keep symmetry in data.
+        naming=self._conf['ftest']['naming']
+        # Raw data if needed.
+        # raw_human_df= self.select_data({'user':user1}, ['user','filename','label', 'u','v'])
+
+        # raw_openp_df= self.select_data({'user':user2}, ['user','filename','label', 'u','v'])
+
+        ftest_pds = pd.DataFrame(columns=['f', 'p_f', 'f accept','s','p_t','t accept'],index=self.unique['columns'],dtype=np.float32)
+        ftest_pds['f accept'] = ftest_pds['f accept'].astype(str)
+        # f_test.cdf(sigma,dfn,dfm)
+        for row in self.unique['columns']:
+            try:
+                # F test
+                human_var = self.error_df.at[row,naming['hv']]
+                openp_var = self.error_df.at[row,naming['ov']]
+                human_deg = self.error_degdf_df.at[row,naming['hd']]
+                openp_deg = self.error_degdf_df.at[row,naming['od']]
+                log.info(row)
+                if human_var>= openp_var:
+                    f = human_var/openp_var
+                else:
+                    f = openp_var/human_var
+                ftest_pds.at[row,'f'] = f
+                p = 1-stats.f.cdf(f, human_deg-1,openp_deg-1)
+                log.info(f"p row={row}, f={f}, p={p},{'Reject' if p < 0.5 else 'Accepted'} ")
+                ftest_pds.at[row,'p_f'] = p
+                if not np.isnan(p):
+                    ftest_pds.at[row,'f accept'] = f"{'Reject' if p < 0.5 else 'Accepted'}"
+
+
+
+            except KeyError as e:
+                breakpoint()
+                raise e
+        # T test
+        breakpoint()
+        temp = stats.ttest_ind(
+                a=self.human_error_df,
+                b=self.openp_error_df,
+                equal_var=True)
+        ftest_pds.style.format("{:.2%}")
+        print(ftest_pds)
+        directions = set(self.unique['imgloc'].values())
+        ftest_pos_df = pd.DataFrame(
+                index=directions,
+                columns=['f','p_f','f accept'])
+        for row in directions:
+            human_mean = self.direction_error_df.at[row,naming['hm']]
+            openp_mean = self.direction_error_df.at[row,naming['om']]
+            human_var  = self.direction_error_df.at[row,naming['hv']]
+            openp_var  = self.direction_error_df.at[row,naming['ov']]
+            log.info(f"{row}-> human:{human_mean,human_var} openp:{openp_mean,openp_var}")
+            human_deg = self.direction_degdf_df.at[row,naming['hd']]
+            openp_deg = self.direction_degdf_df.at[row,naming['od']]
+
+            if human_var>= openp_var:
+                f = human_var/openp_var
+            else:
+                f = openp_var/human_var
+            # ftest_pds.at[row,'f'] = f
+            ftest_pos_df.at[row,'f'] = f
+            p = 1-stats.f.cdf(f, human_deg-1,openp_deg-1)
+            ftest_pos_df.at[row,'p_f'] = p
+            if not np.isnan(p):
+                ftest_pos_df.at[row,'f accept'] = f"{'Reject' if p < 0.5 else 'Accepted'}"
+        print(ftest_pos_df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     def plot3dstats(self, andarg,  alpha=0.0):
@@ -181,7 +510,8 @@ class dataset():
         # First make the image
         df = self.select_data(andarg)
         # fig = plt.Figure(figsize=(15,15))
-        fig = mlab.figure()
+        fig = mlab.figure(bgcolor=(1,1,1))
+
         if conf['showimg']:
             if 'filename' in andarg.keys():
                 setdir = self.setdir
@@ -205,7 +535,7 @@ class dataset():
                 # pp = np.array([0, 0, camera.f])[:,None]
                 # w_pp = camera.w_Rt_c.forward(pp)
                 # obj.actor.position = w_pp.ravel()
-                obj.actor.scale = [0.8, 0.8, 0.8]
+                # obj.actor.scale = [0.8, 0.8, 0.8]
             else:
                 raise IndexError("No filename was declared in andarg keys")
 
@@ -222,6 +552,8 @@ class dataset():
             x_abs = np.max(imgsize.iloc[:,0])
             y_abs = np.max(imgsize.iloc[:,1])
             try:
+                # breakpoint()
+                obj.actor.rotate_z(90)
                 obj.actor.position = [x_abs/2,y_abs/2,0]
             except NameError:
                 log.info("No image showed")
@@ -241,6 +573,7 @@ class dataset():
 
             levels = np.linspace(0,1,40)
 
+            # rv.pdf is the f(x,y)=z function
             z = zscaling*rv.pdf(pos)
 
             # plot the surface
@@ -254,6 +587,7 @@ class dataset():
                     )
             # Set opacity of the surface grid
             grid.actor.property.opacity = 0.5
+            # grid.actor.rotate_z(180)
 
             # Shows the outline box aruond the figure
             mlab.outline(
@@ -261,11 +595,36 @@ class dataset():
                     opacity=0.8
                     )
             # plot the scatter for collected data:
+            print(sample)
+            print(sample.shape)
+            xt = []
+            yt = []
+            zt = []
+            tt = sample/conf['resize']
+            value = [1*conf['markersize'] for _ in np.arange(0,sample.shape[0])]
+            gt = lambda i,j: int(tt.iloc[i][j])
+            w = np.max(imgsize['width'])
+            for i in np.arange(0,sample.shape[0]):
+                xt.append(-1*gt(i,0)*conf['resize'] + w)
+                yt.append(1*gt(i,1)*conf['resize'])
+                zt.append(z[gt(i,0), gt(i,1)])
+
+            # breakpoint()
+            # z = zscaling*rv.pdf(zz)
+            # mlab.points3d(sample['u'],sample['v'],z,value)
+            pplot:mayavi.modules.glyph.Glyph = mlab.points3d(xt,yt,zt,value,scale_factor=1)
+            # breakpoint()
+
+
 
             # Showing or saving the figure
-            mlab.view(azimuth=-30, distance=1e4, roll=-90)
-            mlab.show()
-            # mlab.savefig("../logs/combo_plot_mlab.png")
+            cview = conf['view']
+            mlab.view(
+                    azimuth=cview['azimuth'],
+                    distance=cview['distance'],
+                    roll=cview['roll'])
+            # mlab.show()
+            mlab.savefig("../logs/combo_plot_mlab.png")
 
 
 
@@ -357,14 +716,20 @@ def test_stats(name):
     log.info(f"Test {name}")
     data = dataset(name)
     data.load_anatations()
-    # a = {'filename':'093614.jpg','label':'Nose'}
-    a = {'filename':'093614.jpg'}
-    # print(data.select_data(a))
+    # a = {'user':'Human','label':'Rsholder'}
+    # b = {'user':'OpenPose','label':'Rsholder'}
+    data.error_calulation(0.3)
+    data.error_t_test()
+    # data.t_test()
+    # a = {'filename':'093614.jpg','label':'Nose',' k'
+    # b = {'filename':'093614.jpg','label':'Nose'}
+    # a = {'filename':'093614.jpg'}
+    # # print(data.select_data(a))
     # print("--------------")
     # a = {'filename':'093614.jpg','user':'User1','label':'Nose'}
     # a = {'label':'Nose'}
     # print(data.select_data(a))
-    data.plot3dstats(a)
+    # data.plot3dstats(a)
 
 
 
